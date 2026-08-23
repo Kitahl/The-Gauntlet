2ab86b9193bf8be74803945ec0fa5ac9fe6c6b32
Kitahl <222771578+Kitahl@users.noreply.github.com>
2026-08-23 01:02:32 -0700

    experiment: export vNext6 execution contract

--- experiments/foil_vnext6/__init__.py
+++ experiments/foil_vnext6/__init__.py
@@ -1,5 +1,17 @@
 """FOIL vNext6 composable post-freeze research candidate."""
 
+from .execution_contract import (
+    EvidenceBasis,
+    EvidencePacket,
+    OperatorOutcome,
+    OperatorRequest,
+    OutcomeStatus,
+    OutcomeValidation,
+    ProgressStatus,
+    ToolEffect,
+    build_request,
+    validate_outcome,
+)
 from .runtime_policy import (
     ComposableRuntimePolicy,
     EvidenceAuthority,
@@ -15,11 +27,21 @@ from .runtime_policy import (
 __all__ = [
     "ComposableRuntimePolicy",
     "EvidenceAuthority",
+    "EvidenceBasis",
+    "EvidencePacket",
     "OperatorCost",
+    "OperatorOutcome",
+    "OperatorRequest",
+    "OutcomeStatus",
+    "OutcomeValidation",
+    "ProgressStatus",
     "StrategyBudget",
     "StrategyDecision",
     "StrategyOperator",
     "StrategyTaskContext",
     "TaskComplexity",
+    "ToolEffect",
+    "build_request",
     "strategy_trace",
+    "validate_outcome",
 ]
